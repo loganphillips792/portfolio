eda6be67c4e88390c811387c35168b2ac2c21192
loganphillips792 <loganphillips792@gmail.com>
2022-03-25 23:39:49 -0400

    work experience section

---
+++ components/WorkExperienceSection.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+import Link from 'next/Link';
+import styled from 'styled-components';
+
+const StyledSection = styled.div`
+
+`;
+
+export default function WorkExperienceSection() {
+
+    return (
+        <StyledSection id="experience-section">
+            <h1>Work Experience</h1>
+        </StyledSection>
+    );
+}
