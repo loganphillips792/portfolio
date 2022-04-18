678fe9d77cb2b3bad95731b8643a4fbc12a2189d
loganphillips792 <loganphillips792@gmail.com>
2022-04-17 22:03:45 -0400

    wip - rehype code titles

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -45,6 +45,6 @@ export default makeSource({
   documentTypes: [BlogPost, Project],
   mdx: {
     rehypePlugins: [rehypePrism]
-    // rehypePlugins: [rehypePrism, rehypeCodeTitles]
+    //rehypePlugins: [rehypePrism, rehypeCodeTitles]
   }
 })
