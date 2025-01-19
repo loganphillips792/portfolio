bfa564b670cc00ccdd7c6b87956c8ef2b7860166
loganphillips792 <loganphillips792@gmail.com>
2025-01-19 15:55:09 -0600

    eslint, prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-    extends: 'defaults',
-}
+const js = require('@eslint/js');
+const prettier = require('eslint-plugin-prettier');
+
+module.exports = [
+    js.configs.recommended,
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            globals: {
+                require: 'readonly',
+                __dirname: 'readonly',
+                process: 'readonly',
+                console: 'readonly',
+            },
+            sourceType: 'commonjs', // Treat files as CommonJS
+        },
+    },
+];
